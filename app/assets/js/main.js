da05121f321273d195a099b5abe6c31407a78717
Rafael Oshiro <rafael.oshiro@gmail.com>
2017-01-05 23:31:56 +0000

    Adding search functionality and tweaking css

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,80 +1,95 @@
 (function() {
   window.cache = {}
-  var URL = 'https://athena-7.herokuapp.com/ancients.json';
+  let URL = 'https://athena-7.herokuapp.com/ancients.json';
 
-  function getAncestors(search) {
+  var getAncients = (search) => {
     return $.get(URL, { search: search })
-      .always(function(data) {
+      .always((data) => {
         if(search)
           cacheResultFor(search, data.ancients);
       });
-  };
+  },
 
-  function getError() {
+  getError = () => {
     return $.get(URL, { error: "true" });
-  };
+  },
 
   // Check if local cache has $searchTerm key
-  function hasCachedResultFor(searchTerm) {
+  hasCachedResultFor = (searchTerm) => {
     return cache.hasOwnProperty(searchTerm);
-  };
+  },
 
   // Returns local cache has $searchTerm key
-  function getCachedResultFor(searchTerm) {
-    console.log('retrieving cache');
+  getCachedResultFor = (searchTerm) => {
+    console.log('accessing cache');
     return cache[searchTerm];
-  };
+  },
 
   // Caches the $result, with the $searchTerm as key.
-  function cacheResultFor(searchTerm, result) {
-    console.log('new cache')
+  cacheResultFor = (searchTerm, result) => {
+    console.log('caching result')
     return cache[searchTerm] = result;
-  };
+  },
 
-  function populateGrid(data) {
-    var template = $('#template').html(),
+  populateGrid = (data) => {
+    let template = $('#template').html(),
         rendered = Mustache.render(template, formatAncestorList(data));
 
     $('#target').html(rendered);
+  },
+
+  attachEvents = () => {
+    // Attach click event to search button
+    $('#search-btn').on('click', (e) => {
+      e.preventDefault();
+      let searchTerm = $('#search-term').val();
+      window.ancients.search(searchTerm);
+    });
+    // Attach click event to show error link
+    $('#show-error').on('click', (e) => {
+      window.ancients.showError();
+    });
   };
 
-  function formatAncestorList(ancestors){
-    return ancestors.map(function(item) {
+  function formatAncestorList(list){
+    return (list.ancients || list).map(function(item) {
       return {
         name: item.name.toUpperCase(),
         superpower: item.superpower.toUpperCase(),
         end_of_an_era: new Date(item.end_of_an_era).toLocaleDateString()
       }
     });
-  }
+  };
 
-  window.ancestors = {
+  window.ancients = {
     /**
      * Retrieves a list of ancients and populate the grid with it.
      */
-    init: function() {
-      getAncestors().done(populateGrid);
+    init: () => {
+      attachEvents();
+      getAncients().done(populateGrid);
     },
 
     /**
      * Searches the list of ancients by name.
      * @param: String $searchTerm Term to search for
      */
-    search: function(searchTerm) {
+    search: (searchTerm) => {
       // If search term is already cached, use it.
       if(hasCachedResultFor(searchTerm)) {
         populateGrid(getCachedResultFor(searchTerm));
       } else {
-        getAncestors(searchTerm).done(populateGrid);
+        getAncients(searchTerm).done(populateGrid);
       }
     },
 
     /**
      * Shows the error in the page.
      */
-    showError: function() {
-      getError().done(function(data) {
-        console.log(data);
+    showError: () => {
+      getError().fail((data) => {
+        let parsedResponse = JSON.parse(data.responseText);
+        $('#error-msg').html(parsedResponse.error);
       });
     }
   }
